7e415b4216fd1ee590d2bf967a64a695e19ae1a3
Alex4422 <alexandregautier82@gmail.com>
2020-12-20 21:52:17 +0000

    construction of Main.java which allows to run the class+creation of GamePlay.java Main.java MapGenerator.java

---
+++ com/company/Main.java
@@ -0,0 +1,32 @@
+package com.company;
+
+import javax.swing.*;
+
+/**
+ * Author: Alex
+ * Title of the class: Main
+ * Aim of the class: class which allows to run the game
+ */
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println("Welcome in Zarkanoid!");
+
+        //creation of the window for the game
+        JFrame window = new JFrame();
+        //call of the business logic
+        GamePlay gamePlay = new GamePlay();
+
+        //definitions of the measures of the window
+        window.setBounds(10,10,800,900);
+        //title of the window
+        window.setTitle("Zarkanoid");
+        //interdiction to resize the window
+        window.setResizable(false);
+        //Do we display the window?
+        window.setVisible(true);
+        //When I press the cross, I close the window
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+}
