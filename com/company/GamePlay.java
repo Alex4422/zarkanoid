66b2559eb579879c116d3fb9d371f5efc2ac89e0
Alex4422 <alexandregautier82@gmail.com>
2021-01-04 12:55:50 +0000

    ball & pedal functionnalities : ok + comments + settings of values for config

--- com/company/GamePlay.java
+++ com/company/GamePlay.java
@@ -17,12 +17,18 @@ public class GamePlay extends JPanel implements KeyListener, ActionListener {
 
     //variables for the gameplay
 
-    //The game is started or not ?
     /**
-     *
+     * name: play
+     * type: boolean
+     * description: marker to say us if the th game is started or not
      */
     private boolean play = false;
     //score of the player
+    /**
+     *
+     */
+    private int score = 0;
+    //bricks to shoot
     /**
      *
      */
@@ -62,7 +68,6 @@ public class GamePlay extends JPanel implements KeyListener, ActionListener {
     private int ballYdir = -2;
 
     //constructor of the gameplay, we init the game
-
     /**
      * Element: constructor
      * Title: GamePlay
@@ -82,22 +87,26 @@ public class GamePlay extends JPanel implements KeyListener, ActionListener {
         timer.start();
     }
 
+
     /**
-     * initialisation of the display of the ball, the pedal.
+     * Element: method
+     * Title: paint
+     * Description: initialisation of the display of the ball, the pedal and different of the visual game.
      * We will also control the things here
      * @param g
      */
     public void paint(Graphics g){
 
+        //g.drawString("Hello",100,100);
         //setting of the background
-        g.setColor(Color.GRAY);
+        g.setColor(Color.white);
         g.fillRect(1,1,692,592);
 
         //check!
         g.setColor(Color.yellow);
         g.fillRect(0,0,3,592);
         g.fillRect(0,0,692,3);
-        g.fillRect(691,0,3,562);
+        g.fillRect(691,0,3,592);
 
         //setting of the pedal
         g.setColor(Color.blue);
@@ -107,17 +116,51 @@ public class GamePlay extends JPanel implements KeyListener, ActionListener {
         g.setColor(Color.green);
         g.fillOval(ballposX,ballposY, 20, 20);
 
+        //useful to release the resources
         g.dispose();
+
     }
 
-
     /**
-     *
+     * Element: method
+     * Title: actionPerformed
+     * Description: define the logic, for the movement, of the ball and pedal with limits and
+     * intersections
      * @param actionEvent
      */
     @Override
     public void actionPerformed(ActionEvent actionEvent) {
+        //check the role
+        timer.start();
 
+        //The game is started
+        if(play){
+            if(new Rectangle(ballposX, ballposY, 20, 30).intersects(new Rectangle(playerX, 550, 100, 8))){
+                //If the ball touches the pedal, it bounces
+                ballYdir = -ballYdir;
+            }
+
+            //The ball can move horizontally
+            ballposX += ballXdir;
+            //The ball can move vertically
+            ballposY += ballYdir;
+
+            //If the ball touches the vertical left limit we inverse the direction of the ball
+            if(ballposX < 0){
+                ballXdir = -ballXdir;
+            }
+            //If the ball touches the roof, the top limit of the window, we inverse the direction of the ball
+            if(ballposY < 0){
+                ballYdir = -ballYdir;
+            }
+            //If the ball touches the vertical right limit we inverse the direction of the ball
+            if(ballposX > 670){
+               ballXdir =-ballXdir;
+            }
+        }
+
+        //we refresh the window
+        repaint();
     }
 
     /**
--- com/company/Main.java
+++ com/company/Main.java
@@ -1,6 +1,7 @@
 package com.company;
 
 import javax.swing.*;
+import java.awt.*;
 
 /**
  * Author: Alex
@@ -12,15 +13,41 @@ public class Main {
 
     public static void main(String[] args) {
 
+        //title log
         System.out.println("Welcome in Zarkanoid!");
 
+        //creation of the window for the game
+        JFrame window = new JFrame("Zarkanoid: shot the bricks! ");
+
+        //call of the business logic
+        window.add(new GamePlay());
+
+        //Causes this Window to be sized to fit
+        //the preferred size and layouts of its subcomponents.
+        window.pack();
+
+        //definitions of the measures of the window
+        window.setBounds(10,10,700,600);
+
+        //interdiction to resize the window
+        window.setResizable(false);
+
+        //When I press the cross, I close the window
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        //Do we display the window?
+        window.setVisible(true);
+
+        //check why the code of the author doen't work!
+
+        /*
         //creation of the window for the game
         JFrame window = new JFrame();
         //call of the business logic
         GamePlay gamePlay = new GamePlay();
 
         //definitions of the measures of the window
-        window.setBounds(10,10,800,900);
+        window.setBounds(10,10,700,600);
         //title of the window
         window.setTitle("Zarkanoid");
         //interdiction to resize the window
@@ -31,5 +58,6 @@ public class Main {
         window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
         window.add(gamePlay);
+        */
     }
 }
